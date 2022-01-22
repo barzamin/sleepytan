222bcf1878e7af0e02efb16c1fd555497dd6fe63
Erin Moon <erin@hecke.rs>
2022-01-22 02:30:36 -0500

    basic axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, response::Html, Router};
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HewwoTempl {
+    msg: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(handler));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<String> {
+    let hewwo = HewwoTempl { msg: "uwu".to_owned() };
+    Html(hewwo.render().unwrap())
 }
