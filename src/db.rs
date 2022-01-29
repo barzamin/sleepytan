d7e7b38f84c046ef587b348c23f283661be16d65
Erin Moon <erin@hecke.rs>
2022-01-29 18:53:58 -0500

    accessor insert

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,6 @@
+use argon2::PasswordHasher;
 use color_eyre::eyre::Result;
+use password_hash::PasswordHash;
 use sqlx::SqlitePool;
 
 #[derive(Debug, sqlx::FromRow)]
@@ -6,6 +8,17 @@ pub struct Handle {
     pub id: i64,
     pub accessor: i64,
     pub name: String,
+}
+
+pub async fn insert_accessor<'a>(
+    pool: &'a SqlitePool,
+    passhash: &'a PasswordHash<'a>,
+) -> Result<i64, sqlx::Error> {
+    Ok(sqlx::query("INSERT INTO accessor (passhash) VALUES (?)")
+        .bind(passhash.to_string())
+        .execute(pool)
+        .await?
+        .last_insert_rowid())
 }
 
 pub async fn get_handle(pool: &SqlitePool, id: i64) -> Result<Option<Handle>, sqlx::Error> {
