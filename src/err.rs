2fc182f1fc18ac0eb98b64d44f325d11ca2c06d8
Erin Moon <erin@hecke.rs>
2022-01-31 09:38:52 -0500

    more errtypes

--- src/err.rs
+++ src/err.rs
@@ -3,6 +3,7 @@ use axum::{
     http::StatusCode,
     response::{Html, IntoResponse},
 };
+use color_eyre::eyre;
 
 #[derive(Template)]
 #[template(path = "error.html")]
@@ -19,15 +20,60 @@ impl ErrorTempl {
 #[derive(Debug)]
 pub enum AppError {
     Db(sqlx::Error),
+    PwHash(password_hash::Error),
+    Session(async_session::Error),
+    GenericISE(eyre::Report),
+}
+
+impl From<sqlx::Error> for AppError {
+    fn from(err: sqlx::Error) -> Self {
+        Self::Db(err)
+    }
+}
+
+impl From<password_hash::Error> for AppError {
+    fn from(err: password_hash::Error) -> Self {
+        Self::PwHash(err)
+    }
+}
+
+impl From<async_session::Error> for AppError {
+    fn from(err: async_session::Error) -> Self {
+        Self::Session(err)
+    }
+}
+
+impl From<eyre::Report> for AppError {
+    fn from(err: eyre::Report) -> Self {
+        Self::GenericISE(err)
+    }
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> axum::response::Response {
         let (status, err_resp) = match self {
-            AppError::Db(err) => {
+            Self::Db(err) => {
                 // TODO
-                (StatusCode::INTERNAL_SERVER_ERROR, format!("{}", err))
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("db error: {}", err),
+                )
             }
+            Self::PwHash(err) => {
+                // TODO
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("hashing error: {}", err),
+                )
+            }
+            Self::Session(err) => {
+                // TODO
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("session error: {}", err),
+                )
+            }
+            Self::GenericISE(err) => (StatusCode::INTERNAL_SERVER_ERROR, format!("ISE: {}", err)),
         };
 
         let body = Html(ErrorTempl::new(err_resp).render().unwrap());
