f2f79a68343ca7440671f99db53bb0f458433ede
Erin Moon <erin@hecke.rs>
2022-02-01 16:16:45 -0500

    static file serving

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ use async_session::{async_trait, MemoryStore};
 use axum::{
     extract::{FromRequest, RequestParts},
     response::{Html, IntoResponse, Response},
-    routing::get,
-    AddExtensionLayer, Router,
+    routing::{get, get_service},
+    AddExtensionLayer, Router, http::StatusCode,
 };
 use color_eyre::eyre::Result;
 use sqlx::sqlite::SqlitePool;
 use std::env;
 use tower::ServiceBuilder;
 use tower_cookies::CookieManagerLayer;
+use tower_http::services::ServeDir;
 
 mod data;
 mod db;
@@ -29,6 +30,9 @@ async fn main() -> Result<()> {
     let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
 
     let app = Router::new()
+        .nest("/static", get_service(ServeDir::new("static")).handle_error(|err: std::io::Error| async move {
+            (StatusCode::INTERNAL_SERVER_ERROR, format!("unhandled fs error: {}", err))
+        }))
         .route("/", get(handlers::index::get))
         .route("/:code/", get(handlers::board::get))
         .route("/_/:id", get(handlers::handle::get))
