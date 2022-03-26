3e4711b64abfb87c3d8981daa25ff842a66ef7e6
Erin Moon <erin@hecke.rs>
2022-03-26 18:09:57 -0400

    add board seed sql

---
+++ sql/boards.sql
@@ -0,0 +1,8 @@
+INSERT INTO board (`code`, `desc`)
+VALUES
+    ("b", "brandom"),
+    ("x", "luigi's mansion"),
+    ("v", "ventriloquism"),
+    ("mu", "music"),
+    ("sb", "sleepyboy central"),
+    ("d", "drugs and sleepy paraphernalia");
